c27cb10d7433dac96586fdca99cda13394984a04
gustaveH <ghabiman@gmail.com>
2020-06-16 10:55:15 -0400

    modefied by adding commentdetail component

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className='ui container comments'>
+      <CommentDetail />;
+      <CommentDetail />;
       <CommentDetail />
     </div>
   );
